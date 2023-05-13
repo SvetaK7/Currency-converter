266e7b486ee4ece0b2c640799fa90113d236920d
SvetaK7 <atevs7@inbox.ru>
2023-05-13 23:12:45 +0300

    logic onChangeToPrice

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,46 @@ function App() {
   const [fromPrice, setFromPrice] = useState(0);
   const [toPrice, setToPrice] = useState(0);
 
+  // @ts-ignore
   const onChangeFromPrice = (value: number) => {
     // @ts-ignore
-    const price = value/rates[fromCurrency]
-    // @ts-ignore
-    console.log(price)
-    // @ts-ignore
-    console.log(rates[fromCurrency])
-    console.log(fromCurrency)
-    console.log(value)
+    if (fromCurrency !== 'RUB' && toCurrency !== 'RUB'){
+      // @ts-ignore
+      const price = value/rates[fromCurrency]
+      // @ts-ignore
+      const result = price*rates[toCurrency]
+      setFromPrice(value)
+      setToPrice(result)
+    } else if (toCurrency === 'RUB'){
+      // @ts-ignore
+      const res = value*rates[fromCurrency]
+      setFromPrice(value)
+      setToPrice(res)
+    } else {
+      // @ts-ignore
+      const result = value*rates[toCurrency]
+      setFromPrice(value)
+      setToPrice(result)
+    }
 
-    // @ts-ignore
-    const result = price*rates[toCurrency]
-    setFromPrice(value)
-    setToPrice(result)
   }
   const onChangeToPrice = (value: number) => {
-    setToPrice(value)
+    if (toCurrency !== 'RUB' && fromCurrency !== 'RUB'){
+      // @ts-ignore
+      const result = (rates[fromCurrency]/rates[toCurrency])*value;
+      setFromPrice(result)
+      setToPrice(value)
+    }else if (fromCurrency === 'RUB'){
+      // @ts-ignore
+      const res = value/rates[toCurrency]
+      setToPrice(value)
+      setFromPrice(res)
+    } else {
+      // @ts-ignore
+      const result = value*rates[fromCurrency]
+      setFromPrice(result)
+      setToPrice(value)
+    }
   }
 
 
